88e7ec9720574286825daa48380eb5751d8957d7
Ido Sela <ido.sela@gmail.com>
2013-04-01 10:19:54 -0400

    local commit

--- sync.sh
+++ sync.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
-echo "$(dirname "$0")"
+cd "$(dirname "$0")"
+
+exclude_files=(".DS_Store" "README.md" "sync.sh" ".git")
+files=$(grep -vxF -f <(printf "%s\n" ${exclude_files[@]}) <(ls -A))
+
+# Determine the sync direction
+echo
+echo "Please choose the sync direction:"
+select suite in "To homedir" "From homedir";
+do
+  case "$suite" in
+    "To homedir") syncdir="t";;
+    "From homedir") syncdir="f";;
+    *) echo "Invalid option"; exit 0;;
+  esac
+  break
+done
+
+if [[ $syncdir == "t" ]]; then
+  backupdir="$HOME/dotfiles-backup-$(date +%Y%m%d%H%M%S)"
+  mkdir $backupdir
+
+  for file in ${files[@]}; do
+    file="$HOME/$file"
+    [ -e $file ] && cp $file $backupdir
+    [ -d $file ] && rm -rf $file
+  done
+  unset file
+
+  rsync -av ${files[@]} $HOME/
+else
+  echo "F"
+fi
+
+#     doIt
+#   fi
+#
 # git pull
 # function doIt() {
 # 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
